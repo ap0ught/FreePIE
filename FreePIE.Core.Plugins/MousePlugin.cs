d5f5ea7153b060a2cb4e7f869411bedfa356cdcb
NanoPi <1269605+NanoPi@users.noreply.github.com>
2020-09-09 11:34:36 +1000

    Add mouse.x1Button and mouse.x2Button
    (cherry picked from commit 7418ce39d05e1b7764522d47f343a8f3b96844f9)
    
    Co-authored-by: ap0ught <41078+ap0ught@users.noreply.github.com>

--- FreePIE.Core.Plugins/MousePlugin.cs
+++ FreePIE.Core.Plugins/MousePlugin.cs
@@ -26,6 +26,8 @@ namespace FreePIE.Core.Plugins
         private bool leftPressed;
         private bool rightPressed;
         private bool middlePressed;
+        private bool x1Pressed;
+        private bool x2Pressed;
         private SetPressedStrategy setButtonPressedStrategy;
         private GetHeldDownStrategy<int> getButtonHeldDownStrategy;
 
@@ -190,6 +192,7 @@ namespace FreePIE.Core.Plugins
         public void SetButtonPressed(int index, bool pressed)
         {
             uint btn_flag = 0;
+            uint btn_data = 0;
             if (index == 0)
             {
                if (pressed)
@@ -218,6 +221,47 @@ namespace FreePIE.Core.Plugins
                }
                rightPressed = pressed;
             }
+            else if (index == 3)
+            {
+                if (pressed)
+                {
+                    if (!x1Pressed)
+                    {
+
+                        btn_data = MouseKeyIO.XBUTTON1;
+                        btn_flag = MouseKeyIO.MOUSEEVENTF_XDOWN;
+                    }
+                }
+                else
+                {
+                    if (x1Pressed)
+                    {
+                        btn_data = MouseKeyIO.XBUTTON1;
+                        btn_flag = MouseKeyIO.MOUSEEVENTF_XUP;
+                    }
+                }
+                x1Pressed = pressed;
+            }
+            else if (index == 4)
+            {
+                if (pressed)
+                {
+                    if (!x2Pressed)
+                    {
+                        btn_data = MouseKeyIO.XBUTTON2;
+                        btn_flag = MouseKeyIO.MOUSEEVENTF_XDOWN;
+                    }
+                }
+                else
+                {
+                    if (x2Pressed)
+                    {
+                        btn_data = MouseKeyIO.XBUTTON2;
+                        btn_flag = MouseKeyIO.MOUSEEVENTF_XUP;
+                    }
+                }
+                x2Pressed = pressed;
+            }
             else
             {
                if (pressed)
@@ -236,7 +280,7 @@ namespace FreePIE.Core.Plugins
             if (btn_flag != 0) {
                var input = new MouseKeyIO.INPUT[1];
                input[0].type = MouseKeyIO.INPUT_MOUSE;
-               input[0].mi = MouseInput(0, 0, 0, 0, btn_flag);
+               input[0].mi = MouseInput(0, 0, btn_data, 0, btn_flag);
             
                MouseKeyIO.SendInput(1, input, Marshal.SizeOf(input[0].GetType()));
             }
@@ -314,6 +358,18 @@ namespace FreePIE.Core.Plugins
         {
             get { return plugin.IsButtonDown(1); }
             set { plugin.SetButtonPressed(1, value); }
+        }
+
+        public bool x1Button
+        {
+            get { return plugin.IsButtonDown(3); }
+            set { plugin.SetButtonPressed(3, value); }
+        }
+
+        public bool x2Button
+        {
+            get { return plugin.IsButtonDown(4); }
+            set { plugin.SetButtonPressed(4, value); }
         }
 
         public bool getButton(int button)
