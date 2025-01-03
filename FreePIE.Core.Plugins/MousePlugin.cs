04b4e4e05d6674243a5010664bbdc61ef7121b45
JabberwockPL <unconfigured@null.spigotmc.org>
2025-01-03 14:36:57 +0100

    Absolute mouse coordinates
    (cherry picked from commit f6a37023e6d4f1c8577bf1e7d29a973e81544353)
    
    Co-authored-by: ap0ught <41078+ap0ught@users.noreply.github.com>

--- FreePIE.Core.Plugins/MousePlugin.cs
+++ FreePIE.Core.Plugins/MousePlugin.cs
@@ -99,7 +99,7 @@ namespace FreePIE.Core.Plugins
                 else
                 {
                     input[0].mi = MouseInput((int)deltaXOut, (int)deltaYOut, (uint)wheel, 0, MouseKeyIO.MOUSEEVENTF_MOVE | MouseKeyIO.MOUSEEVENTF_WHEEL);
-                 }
+                }
 
                 MouseKeyIO.SendInput(1, input, Marshal.SizeOf(input[0].GetType()));
 
